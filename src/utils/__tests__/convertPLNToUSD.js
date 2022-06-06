a5f1c7ef6afd1ba281a0b9b79fd2df992e04b062
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-06-06 12:24:44 +0200

    Add tests to convertPLtoUSD function

--- src/utils/__tests__/convertPLNToUSD.js
+++ src/utils/__tests__/convertPLNToUSD.js
@@ -7,4 +7,23 @@ describe('ConvertPLNtoUSD', () => {
     expect(convertPLNToUSD(20)).toBe('$5.71');
     expect(convertPLNToUSD(12)).toBe('$3.43');
   });
+  it('should return NaN when input is text', () => {
+    expect(convertPLNToUSD('6')).toBeNaN();
+    expect(convertPLNToUSD('abc')).toBeNaN();
+    expect(convertPLNToUSD('-87')).toBeNaN();
+  });
+  it('should return NaN when no input', () => {
+    expect(convertPLNToUSD('')).toBeNaN();
+  });
+  it('should return string "Error" when input is not a number nor astring', () => {
+    expect(convertPLNToUSD([])).toBe('Error');
+    expect(convertPLNToUSD({})).toBe('Error');
+    expect(convertPLNToUSD(null)).toBe('Error');
+    expect(convertPLNToUSD(function() {})).toBe('Error');
+  });
+  it('should return $0.00 when input is a number < 0', () => {
+    expect(convertPLNToUSD(-1)).toBe('$0.00');
+    expect(convertPLNToUSD(-19)).toBe('$0.00');
+    expect(convertPLNToUSD(-78643)).toBe('$0.00');
+  });
 });
